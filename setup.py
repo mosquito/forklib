5baec9f43e7562f4a707e8a100719f3c7aa421e6
Dmitry Orlov <me@mosquito.su>
2018-07-25 17:10:06 +0300

    [fix] bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='forklib',
-    version='0.1.0',
+    version='0.2.0',
     platforms="posix",
     author="Dmitry Orlov",
     author_email="me@mosquito.su",
