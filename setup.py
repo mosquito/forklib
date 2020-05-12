f719140c0ce3cd35c814cd181f02310359ebc804
Dmitry Orlov <me@mosquito.su>
2020-05-12 12:38:15 +0300

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="forklib",
-    version="0.3.0",
+    version="0.4.0",
     platforms="posix",
     author="Dmitry Orlov",
     author_email="me@mosquito.su",
